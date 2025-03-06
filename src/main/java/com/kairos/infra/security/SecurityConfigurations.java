fa35acc2fa8abcec62cf1ccdf93dad245f8c4f0a
Lucca Lucchin de Campos Costa <lucca.lucchin@gmail.com>
2025-03-06 04:00:02 -0300

    fixed 403 code appearing instead of 401

--- src/main/java/com/kairos/infra/security/SecurityConfigurations.java
+++ src/main/java/com/kairos/infra/security/SecurityConfigurations.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -15,17 +16,17 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
 	
 	@Autowired
 	SecurityFilter securityFilter;
+	
+	@Autowired
+	CustomAccessDeniedHandler accessDeniedHandler;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -38,6 +39,8 @@ public class SecurityConfigurations {
 						.requestMatchers(HttpMethod.POST, "/user").hasRole("ADMIN")
 						.anyRequest().authenticated()
 				)
+				.exceptionHandling(e -> e.accessDeniedHandler(accessDeniedHandler)
+						.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
 				.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
 				.build();
 	}
